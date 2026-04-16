a033e633c429a1a16938800d885f777e449b7ceb
Jinjie Liu <jjliu@baai.ac.cn>
2026-04-16 14:43:33 +0800

    refactor(libtriton-core): use upstream torch-mlir sources and rename .cpp to .cc
    Signed-off-by: Jinjie Liu <jjliu@baai.ac.cn>

--- libtriton-core/python/RegistrationModules.cc
+++ libtriton-core/python/RegistrationModules.cc
@@ -1,10 +1,7 @@
-#include <cstdint>
-
 #include "libtriton-core-c/Dialects.h"
 #include "libtriton-core-c/Registration.h"
 #include "mlir/Bindings/Python/NanobindAdaptors.h"
 #include "torch-mlir-c/Dialects.h"
-#include "torch-mlir-c/Registration.h"
 
 namespace nb = nanobind;
 
@@ -41,19 +38,4 @@ NB_MODULE(_libtritonCore, m) {
   m.def("register_all_dialects", &registerAllDialects, nb::arg("context"),
         nb::arg("load") = true);
   m.def("register_all_passes", &registerAllPasses);
-}
-
-NB_MODULE(_torchMlirCore, m) {
-  torchMlirRegisterAllPasses();
-
-  m.doc() = "libtriton-core torch registration extension";
-
-  m.def(
-      "register_dialect",
-      [](MlirContext context, bool load) {
-        registerDialect(mlirGetDialectHandle__torch__(), context, load);
-      },
-      nb::arg("context"), nb::arg("load") = true);
-  m.def("get_int64_max", []() { return INT64_MAX; });
-  m.def("get_int64_min", []() { return INT64_MIN; });
 }
--- libtriton-core/python/libtriton_core/__init__.py
+++ libtriton-core/python/libtriton_core/__init__.py
@@ -3,17 +3,7 @@ import pkgutil
 __path__ = pkgutil.extend_path(__path__, __name__)
 
 from ._mlir_libs._libtritonCore import register_all_dialects, register_all_passes
-from . import capi_utils, execution_engine, ir, passmanager, rewrite
-
-try:
-    from . import compiler_utils
-except ImportError:
-    compiler_utils = None
-
-try:
-    from . import fx
-except ImportError:
-    fx = None
+from . import capi_utils, compiler_utils, execution_engine, fx, ir, passmanager, rewrite
 
 __all__ = [
     "capi_utils",
